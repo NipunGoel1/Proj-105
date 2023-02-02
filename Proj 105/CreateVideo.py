5b2ff7afc015016508f6bd080e21bd482a4b3a31
NipunGoel1 <66476112+NipunGoel1@users.noreply.github.com>
2023-02-02 23:33:43 +0530

    Add files via upload

---
+++ Proj 105/CreateVideo.py
@@ -0,0 +1,34 @@
+import os 
+import cv2
+
+path = "Images"
+images = []
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+        print(file_name)
+        images.append(file_name)
+print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,channels = frame.shape
+size = (width, height)
+print(size)
+
+
+out = cv2.VideoWriter("project.mp4",cv2.VideoWriter_fourcc(*'DIVX'),5,size)
+for i in range (count-1,0,-1):
+    frame = cv2.imread(images[i])
+    cv2.imshow("webcam", frame)
+    out.write(frame)
+
+    if cv2.waitKey(750) ==820:
+            break
+
+
+out.release()
+cv2.destroyAllWindows()
+
+print("WE DID IT BOIS EZZZZZZZZZZZ!")
